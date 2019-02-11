5a4d2b992c85437c9dfbc3d9abdaae36f10059e7
엄기종(Kijong Uhm) <kjeom@humaxdigital.com>
2019-02-11 14:02:42 +0900

    5.1 VideoView

--- ysdm.fg.app/src/main/java/com/example/kjeom/ysdm_01/MediaActivity.java
+++ ysdm.fg.app/src/main/java/com/example/kjeom/ysdm_01/MediaActivity.java
@@ -2,6 +2,9 @@ package com.example.kjeom.ysdm_01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MediaActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MediaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
+        initE51();
+    }
+
+    /*
+     * E5.1
+     */
+    private VideoView mVideo;
+    private MediaController mMediaController;
+    boolean isPlaying1 = false;
+
+    private void initE51() {
+        mVideo=(VideoView) findViewById(R.id.videoView);
+        MediaController mMediaController = new MediaController(this);
+        mMediaController.setAnchorView(mVideo);
+        mVideo.setMediaController(mMediaController);
+        mVideo.setKeepScreenOn(true);
+        mVideo.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.jack_reacher_trailer_2012);
+    }
+    public void onE51Play(View view) {
+        if (isPlaying1 == false) {
+            mVideo.start();
+            isPlaying1 = true;
+        }
+    }
+
+    public void onE51Stop(View view) {
+        mVideo.stopPlayback();
+        isPlaying1 = false;
     }
 }
